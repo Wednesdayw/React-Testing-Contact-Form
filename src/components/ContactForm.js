bdf9555a8e76021c90d6bb2fc3ec6382388df48c
Melissa C <66797764+Wednesdayw@users.noreply.github.com>
2020-09-04 09:03:02 -0400

    Testing, it's working!
    Comments and notes included

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-
+/*Notes:
+Change inputs to include id
+Changed maxLength to minLength in firstName input
+data test results*/
 const ContactForm = () => {
   const [data, setData] = useState();
   const { register, errors, handleSubmit } = useForm({
@@ -16,9 +19,10 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id="firstName"
             name="firstName"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true, minLength: 3 })} 
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -28,6 +32,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -41,17 +46,17 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id="email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
-        </div>
+          <textarea id="message" name="message" ref={register({ required: false })} /> 
+        </div> 
         {data && (
-          <pre style={{ textAlign: "left", color: "white" }}>
+          <pre data-testid="results" style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen, fireEvent } from "@testing-library/react"; //import test
+import ContactForm from './ContactForm'; //import form
+
+test('ContactForm registers new users', async() => {
+    render(<ContactForm />);
+
+    // inputs: firstName, lastName, email, message
+    const firstNameInput = screen.getByLabelText(/First Name*/i);
+    const lastNameInput = screen.getByLabelText(/Last Name*/i);
+    const emailInput = screen.getByLabelText(/Email/i);
+    const messageInput = screen.getByLabelText(/Message/i);
+
+    // events with RTL 
+    fireEvent.change(firstNameInput, { target: { value: 'Lauren' } });
+    fireEvent.change(lastNameInput, { target: { value: 'Emick' } });
+    fireEvent.change(emailInput, { target: { value: 'lauren@lauren.com' } });
+    fireEvent.change(messageInput, { target: { value: 'Hello World' } });
+
+    // submit button
+    const submitButton = screen.getByRole('button');
+    fireEvent.click(submitButton);
+
+    // assert that new user is submitted
+    const results = await screen.findByTestId('results');
+    expect(results).toBeInTheDocument();
+    // console.log(results);
+
+});
